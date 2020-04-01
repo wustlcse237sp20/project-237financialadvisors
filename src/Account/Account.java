1e768c89d9d1048407d06087c9589348365a2ea6
nicolecook <nicolecook@192.168.1.3>
2020-03-31 19:30:40 -0500

    started interface

--- src/Account/Account.java
+++ src/Account/Account.java
@@ -1,5 +1,10 @@
 package Account;
 
 public interface Account {
-
+	public String getNameOfBank();
+	public String getAccountType(); // not sure about type?
+	public double getBalance();
+	public double getInterestRate();
+	public boolean deposit();
+	public boolean withdraw();
 }
