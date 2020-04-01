08ebe9dcaa2fc51c873eabc051f4973d9be5abf7
nicolecook <nicolecook@192.168.1.3>
2020-04-01 11:35:47 -0500

    one account class instead of separate types, need to update branch

--- src/Account/Account.java
+++ src/Account/Account.java
@@ -1,10 +1,55 @@
 package Account;
 
-public interface Account {
-	public String getNameOfBank();
-	public String getAccountType(); // not sure about type?
-	public double getBalance();
-	public double getInterestRate();
-	public boolean deposit();
-	public boolean withdraw();
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+
+	// List<String> Accounts = new ArrayList<String>();
+
+private String bankName;
+private String accountType;
+private double accountBalance;
+private double interestRate;
+private double overdrawAllowed;
+
+public Account(String accountType, String bankName, double interestRate, double overdrawAllowed) {
+	this.accountType = accountType;
+	this.bankName = bankName;
+	this.interestRate = interestRate;
+	this.overdrawAllowed = overdrawAllowed;
+}
+
+	public String getNameOfBank() {
+		return bankName;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public double getBalance() {
+		return accountBalance; // this?
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public boolean deposit(double amount) {
+		if (amount > 0.0) {
+			accountBalance = accountBalance + amount;
+			return true;
+	    }
+		return false;
+	}
+
+	public boolean withdraw(double amount) {
+		if ((amount > 0.0) && ((accountBalance - amount) > (this.overdrawAllowed)*-1)) {
+			accountBalance = accountBalance - amount;
+			return true;
+		}
+		return false;
+	}
+
 }
---
+++ test/AccountTests.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Account.Account;
+
+public class AccountTests {
+
+	@Test
+	public void testWithdraw() {
+		Account a = new Account("Saving", "Chase", 0.1, 500.0);
+		//allowed to overdraw 500
+		assertTrue(a.withdraw(500.0));
+		//can't overdraw more than ____ (assuming 1000 is too much to overdraw)
+		assertFalse(a.withdraw(1.0));
+		a.deposit(1000.0);
+		double[] withdrawAmounts = {2.0, -5.0, 600.0, 2000.0};
+		boolean [] withdrawSuccess = {true, false, true, false};
+		for(int i=0;i<withdrawAmounts.length; i++) {
+			assertEquals(withdrawSuccess[i], a.withdraw(withdrawAmounts[i]));
+		}
+	}
+	
+	public void testDeposit() {
+		Account b = new Account("Checking", "Bank of America", 0.0, 25);
+		b.deposit(1000.0);
+		assertTrue(b.deposit(100.0));
+		assertFalse(b.deposit(-100.0));
+		
+	}
+
+}
