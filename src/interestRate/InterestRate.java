4be59c1d647faac0e49199ef811597c84c6a9009
sattieee <60641087+sattieee@users.noreply.github.com>
2020-04-04 19:08:29 -0500

    Interest Rate calculator - updated

--- src/interestRate/InterestRate.java
+++ src/interestRate/InterestRate.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 
 public class InterestRate {
  
-	
-	public void interestRateCalculator (double accountBalance, double interestRate, int years) {
+	/**
+	 * 
+	 * @param accountBalance balance in the account in question
+	 * @param interestRate interest rate for account
+	 * @param years number of years for which interest rate is applied
+	 * @param compoundPerYear number of times interest rate is applied per year
+	 */
+	public void interestRateCalculator (double accountBalance, double interestRate, int years, int compoundPerYear) {
 		
 		double principalBalance = accountBalance;
 		
 		for (int i = 0; i < years; i++){ 
 			
-			accountBalance = principalBalance * Math.pow((1 + interestRate), years);		
+			accountBalance = principalBalance * Math.pow((1 + (interestRate/compoundPerYear)), years);		
 		}
-		
 		
 	}
 	
