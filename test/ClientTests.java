0de876d711391b89e7d4f08cb1701a9de42eb550
nicolecook <nicolecook@192.168.1.3>
2020-04-10 15:14:57 -0500

    transfer money, add account, delete account methods added

--- test/ClientTests.java
+++ test/ClientTests.java
@@ -13,24 +13,30 @@ import Account.Account;
 import Account.Client;
 
 public class ClientTests {
+	List<Account> Accounts;
+	Account a;
+	Account b;
+	Account c;
+	Account d;
+	Account e;
 	Client one;
 	
 	@Before
 	public void setUp() {
-		List<Account> Accounts = new ArrayList<Account>();
-		Account a = new Account("savings", "chase", 0.2, 500.0, 12345678);
+		Accounts = new ArrayList<Account>();
+		a = new Account("savings", "chase", 0.2, 500.0, 12345678);
 		a.deposit(500.0);
 		Accounts.add(a);
-		Account b = new Account("checkings", "boa", 0.0, 100.0, 87654321);
+		b = new Account("checkings", "boa", 0.0, 100.0, 87654321);
 		b.deposit(300.0);
 		Accounts.add(b);
-		Account c = new Account("investments", "fidelity", 7.0, 0.0, 56781234);
+		c = new Account("investments", "fidelity", 7.0, 0.0, 56781234);
 		c.deposit(700.0);
 		Accounts.add(c);
-		Account d = new Account("bonds", "etrade", 3.0, 0.0, 43218765);
+		d = new Account("bonds", "etrade", 3.0, 0.0, 43218765);
 		d.deposit(300.0);
 		Accounts.add(d);
-		Account e = new Account("investments", "etrade", 7.0, 0.0, 34561278);
+		e = new Account("investments", "etrade", 7.0, 0.0, 34561278);
 		e.deposit(200.0);
 		Accounts.add(e);
 		one = new Client(Accounts, 27);
@@ -66,6 +72,19 @@ public class ClientTests {
 			e.printStackTrace();
 		}
 		ps.close();
+	}
+	
+	@Test
+	public void testTransferMoney() {
+		one.transferMoney(12345678, 87654321, 100.0);
+		assertTrue(a.getBalance()==400.0);
+		assertTrue(b.getBalance()==200.0);
+	}
+	
+	@Test
+	public void testDeleteAccount() {
+		one.deleteAccount(34561278);
+		assertTrue(Accounts.size()==4);
 	}
 	
 //	@Test
